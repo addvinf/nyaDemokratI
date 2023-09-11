e05fa322a244a867f008f4c8bbd4be25618e549e
addvinf <91795700+addvinf@users.noreply.github.com>
2023-09-11 16:03:51 +0200

    jobbar på admin

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import FullForm from './components/FullForm'
+import Admin from './components/Admin'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <div>
       <Header /> 
       <FullForm />
+      <Admin />
        
   
     </div>
