42981e1ee44c927c29d2ffe55054918ede533385
addvinf <91795700+addvinf@users.noreply.github.com>
2023-09-11 13:57:41 +0200

    nu funkar det

--- vite-project/src/components/Functions.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import VoteButton from "./VoteButton"; "./VoteButton.jsx"
-import VotingForm from "./VotingForm"; "./VotingForm.jsx"
-
-function buttonClick() {
-    console.log("Du har röstat på " + VoteButton.name);
-
-}
