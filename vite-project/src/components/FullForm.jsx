beb950d871f5e215ab07163feb6835da437ac06b
mdslty <mats.desaulty@gmail.com>
2023-09-11 14:27:37 +0200

    fan

--- vite-project/src/components/FullForm.jsx
+++ vite-project/src/components/FullForm.jsx
@@ -7,11 +7,21 @@ export default function FullForm(props) {
     const [email, setEmail] = useState("");
     const [selectedCandidate, setSelectedCandidate] = useState(null);
     console.log(email + " " + selectedCandidate);
+
+    function hash(string){
+        var hash = 0;
+        for (var i = 0; i < string.length; i++) {
+            hash += Math.pow(string.charCodeAt(i) * 31, string.length - i);
+            hash = hash & hash; // Convert to 32bit integer
+        }
+        return hash;
+    }
     
     function submitVote(event) {
         event.preventDefault(); // Prevent the default form submission behavior
         if (selectedCandidate !== null) {
-            console.log("Voted for:", selectedCandidate + " with email: " + email);
+            var userId = hash(email);
+            console.log("Voted for:", selectedCandidate + " with email: " + email + " and userId: " + userId);
             // You can send the selected candidate to the server or perform other actions here
         } else {
             console.log("Please select a candidate before submitting.");
