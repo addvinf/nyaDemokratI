81a2061b7ccad8af18c99242a4e2602802785ae0
addvinf <91795700+addvinf@users.noreply.github.com>
2023-09-13 15:11:33 +0200

    buggfix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { useState } from 'react'
+//Den här är trasig vet inte varför
+//import { UserListProvider } from './context/UserListContext';
 
 import './App.css'
 
 import Admin from './routes/Admin'
-
-
 import User from './routes/User'
 
 function App() {
---
+++ vite-project/src/context/UserListContext.js
@@ -0,0 +1,17 @@
+import { useState, createContext } from "react";
+
+export const UserListContext = createContext();
+
+export const UserListProvider = (props) => {
+    const [userList, setUserList] = useState([]);
+
+
+
+
+    return (
+        <UserListContext.Provider value={{ userList, setUserList }}>
+            {props.children}
+        </UserListContext.Provider>
+    )
+
+}
