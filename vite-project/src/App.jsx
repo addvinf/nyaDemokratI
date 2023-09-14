2cf63fcc8568ac47ff3f9e4b14375e7ae077f1a7
mdslty <mats.desaulty@gmail.com>
2023-09-14 15:04:58 +0200

    Commit app change

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,20 +2,25 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { useState } from 'react'
 //Den här är trasig vet inte varför
 import { UserListProvider } from './context/UserListContext';
+import { AuthProvider } from './AuthContext';
 
 import './App.css'
 
 import Admin from './routes/Admin'
 import User from './routes/User'
+import LockedAdmin from './routes/LockedAdmin'
+import UnlockedAdmin from './routes/UnlockedAdmin'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
     <UserListProvider>
       <Router>
         <Routes>
           <Route exact path="/" element={<User/>}/>
+          <Route exact path="/lockedadmin" element={<LockedAdmin/>}/>
+          <Route exact path="/unlockedadmin" element={<UnlockedAdmin/>}/>
           <Route exact path="/admin" element={<Admin/>}/>
         </Routes>       
     </Router>
@@ -25,4 +30,10 @@ function App() {
   )
 }
 
-export default App
+export default function WrappedApp() {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+}
